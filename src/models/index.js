e25bd29d4330f5646cec0113cd5be2ae4a746fae
lianavaleria15 <liana.valeria15@gmail.com>
2021-12-26 15:06:48 -0800

    added models associations

--- src/models/index.js
+++ src/models/index.js
@@ -4,13 +4,21 @@ const Blog = require("./Blog");
 const Comment = require("./Comment");
 
 //blog belongs to user
+Blog.belongsTo(User, { foreignKey: "user_id" });
 
 //user has many blogs
+User.hasMany(Blog, { foreignKey: "user_id" });
 
 //comment belongs to blog
+Comment.belongsTo(Blog, { foreignKey: "blog_id" });
 
 //blog has many comments
+Blog.hasMany(Comment, { foreignKey: "blog_id" });
 
 //user has many comments
+User.hasMany(Comment, { foreignKey: "user_id" });
 
 //comment belongs to user
+Comment.belongsTo(User, foreignKey("user_id"));
+
+module.exports = { User, Blog, Comment };
