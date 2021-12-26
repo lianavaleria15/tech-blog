35d2880ee1dabfaf76bccf8a57ee46ad87dd813e
lianavaleria15 <liana.valeria15@gmail.com>
2021-12-26 15:00:04 -0800

    added associations models pseudocode

---
+++ src/models/index.js
@@ -0,0 +1,16 @@
+//import models
+const User = require("./User");
+const Blog = require("./Blog");
+const Comment = require("./Comment");
+
+//blog belongs to user
+
+//user has many blogs
+
+//comment belongs to blog
+
+//blog has many comments
+
+//user has many comments
+
+//comment belongs to user
