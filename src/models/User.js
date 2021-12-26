df9816f37fef215b2fb696ca7dba075a29227d36
lianavaleria15 <liana.valeria15@gmail.com>
2021-12-26 14:24:16 -0800

    added user model, hooked password and config code

--- src/models/User.js
+++ src/models/User.js
@@ -1,2 +1,43 @@
 const { Model, DataTypes } = rquire("sequelize");
 const bcrypt = require("bcrypt");
+const sequelize = require("bcrypt");
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: { len: [8] },
+  },
+};
+
+const options = {
+  hooks: {
+    beforeCreate: async (newUserData) => {
+      newUserData.password = await bcrypt.hash(newUserData.password, 10);
+      return newUserData;
+    },
+  },
+
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "user",
+};
+
+User.init(schema, options);
+
+module.exports = User;
