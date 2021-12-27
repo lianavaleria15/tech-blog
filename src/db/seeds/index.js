b238f9008c86e5c6dcb3cf8d0150dccf36353521
lianavaleria15 <liana.valeria15@gmail.com>
2021-12-26 16:06:02 -0800

    added fs to seed models data

--- src/db/seeds/index.js
+++ src/db/seeds/index.js
@@ -11,3 +11,22 @@ const blogData = require("./blog-seeds.json");
 const commentData = require("./comment-seeds.json");
 
 //seed data
+const seedData = async () => {
+  await sequelize.sync({ force: true });
+
+  //seed user data
+  await User.bulkCreate(userData, { individualHooks: true, returning: true });
+
+  //seed blog post data
+  await Blog.bulkCreate(blogData, { individualHooks: true, returning: true });
+
+  //seed comments data
+  await Comment.bulkCreate(commentData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedData();
